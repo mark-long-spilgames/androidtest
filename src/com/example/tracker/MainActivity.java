10db76fdf681b5d1cf8cc1dabd5b1918bb7a6b01
Mark Long <mark.long@spilgames.com>
2013-07-31 22:10:39 +0200

    first network sending data

--- src/com/example/tracker/MainActivity.java
+++ src/com/example/tracker/MainActivity.java
@@ -1,11 +1,19 @@
 package com.example.tracker;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import android.app.Activity;
-import android.content.Intent;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
-import android.widget.EditText;
 
 public class MainActivity extends Activity {
 	
@@ -25,12 +33,63 @@ public class MainActivity extends Activity {
         return true;
     }
     
-    public void sendMessage(View view) {
-    	Intent intent = new Intent(this, StartTrackingActivity.class);
-    	EditText editText = (EditText) findViewById(R.id.edit_message);
-    	String message = editText.getText().toString();
-    	intent.putExtra(EXTRA_MESSAGE, message);
-    	startActivity(intent);
+	public void startTracking(View view) {
+
+		ConnectivityManager connMgr = (ConnectivityManager) 
+				getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+		
+		if (networkInfo != null && networkInfo.isConnected()) {
+			new DownloadWebpageTask().execute("http://api.baobao365.eu/?pos=android");
+		} else {
+			Log.i("test", "no network");
+		}
+
+	}
+	
+    private class DownloadWebpageTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+              
+            // params comes from the execute() call: params[0] is the url.
+            try {
+                return downloadUrl(urls[0]);
+            } catch (IOException e) {
+                return "Unable to retrieve web page. URL may be invalid.";
+            }
+        }
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+            //textView.setText(result);
+       }
     }
-    
+
+    private  String downloadUrl(String myurl) throws IOException {
+        InputStream is = null;
+        try {
+            URL url = new URL(myurl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000 /* milliseconds */);
+            conn.setConnectTimeout(15000 /* milliseconds */);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            // Starts the query
+            conn.connect();
+            int response = conn.getResponseCode();
+            Log.d("test", "The response is: " + response);
+            is = conn.getInputStream();
+
+            // Convert the InputStream into a string
+            //String contentAsString = readIt(is, len);
+            return "true";
+            
+        // Makes sure that the InputStream is closed after the app is
+        // finished using it.
+        } finally {
+            if (is != null) {
+                is.close();
+            } 
+        }
+    }
 }
